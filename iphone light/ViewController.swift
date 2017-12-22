d1efe247cfd02576bfde0d67cabb368493d01cb8
student <student@productservice.ttknn.net>
2017-12-22 11:52:36 +0300

    Initial Commit

---
+++ iphone light/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iphone light
+//
+//  Created by student on 22.12.2017.
+//  Copyright © 2017 Seleznev Nikolai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
